345c73c680a9d1eb0fdaa6dd97c574593440b728
Matthieu UHRY <matthieuuhry@wifi-eduroam-interne-172-29-113-146.wifi.unistra.fr>
2025-11-14 16:09:10 +0100

    Initial commit

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'UHRY_MUGNIER_Info_Indus_projet'
+copyright = '2025, UHRY MUGNIER'
+author = 'UHRY MUGNIER'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
